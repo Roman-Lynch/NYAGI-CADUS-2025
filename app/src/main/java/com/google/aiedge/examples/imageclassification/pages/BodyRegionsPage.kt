6ee03e8ce2df52e5872b0b638652d2b2e700cfa1
Ethan Nguyen <etng4880@colorado.edu>
2025-02-26 23:33:40 -0700

    partially built scan option tile, updated gitignore slightly, uploading image classification images

--- app/src/main/java/com/google/aiedge/examples/imageclassification/pages/BodyRegionsPage.kt
+++ app/src/main/java/com/google/aiedge/examples/imageclassification/pages/BodyRegionsPage.kt
@@ -1,9 +1,89 @@
 package com.google.aiedge.examples.imageclassification.pages
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import coil.size.Size.Companion.ORIGINAL
+import com.google.aiedge.examples.imageclassification.view.Theme
+
+// include all implemented model options here
+private val options:List<String> = listOf("Breast", "Pregnancy", "Early Pregnancy", "Late Pregnancy", "Shoulder")
 
 @Composable
-fun BodyRegionsPage() {
-    Text("Body Regions Page Goes Here")
+fun SelectorOption(optionName:String){
+    val painter = rememberAsyncImagePainter(
+        model = ImageRequest.Builder(LocalContext.current)
+            .data("file:///android_asset/ModelSelectionIcons/${optionName}.png")
+            .size(ORIGINAL)
+            .build(),
+        contentScale = ContentScale.FillBounds
+    )
+//    AsyncImage(
+//        model = "file:///android_asset/ModelSelectionIcons/${optionName}.png",
+//        contentDescription = "Image Representing ${optionName}",
+//        modifier = Modifier
+//            .size(300.dp)
+//    )
+    Box(Modifier
+        .height(300.dp)
+        .width(300.dp)
+        .clip(RoundedCornerShape(10.dp))
+        .background(Theme.NyagiPurple)
+        .paint(painter, contentScale = ContentScale.FillBounds)
+        .border(3.dp, Theme.Black, shape = RoundedCornerShape(10.dp))
+    ) {
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .border(3.dp, Theme.Black, shape = RoundedCornerShape(10.dp)),
+
+        ){
+            Text("Test text String")
+        }
+    }
+}
+
+@Composable
+fun OptionsGrid(){
+    LazyVerticalGrid(columns = GridCells.Fixed(2)) {
+        items(options){ option ->
+            SelectorOption(option)
+        }
+    }
+}
+
+// modifier here is standard modifier that applies to every page
+@Composable
+fun BodyRegionsPage(modifier: Modifier) {
+    Box(modifier = modifier){
+//        OptionsGrid()
+        SelectorOption("Breast")
+    }
+}
+
+@Preview
+@Composable
+fun Preview(){
+    SelectorOption("Early Pregnancy")
 }
