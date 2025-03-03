c53d67c34cd5b2c3191acae8b6e930acce905c17
Ethan Nguyen <etng4880@colorado.edu>
2025-03-03 03:12:54 -0700

    cleaned up navbar

--- app/src/main/java/com/google/aiedge/examples/imageclassification/view/DevelopmentTab.kt
+++ app/src/main/java/com/google/aiedge/examples/imageclassification/view/DevelopmentTab.kt
@@ -4,28 +4,21 @@ import androidx.compose.foundation.background
 import androidx.camera.core.ImageProxy
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.google.aiedge.examples.imageclassification.UiState
 import com.google.aiedge.examples.imageclassification.language.Language
 import com.google.aiedge.examples.imageclassification.pages.BodyRegionsPage
 import com.google.aiedge.examples.imageclassification.pages.SettingsPage
 import com.google.aiedge.examples.imageclassification.pages.BreastCameraPage
-import com.google.aiedge.examples.imageclassification.*
 
 private val horizontalPadding: Dp = 25.dp // standard margins for page
 private val standardModifier:Modifier = Modifier.padding(horizontal = horizontalPadding)
@@ -60,23 +53,15 @@ fun DefaultAlert(onClick: () -> Unit) {
 fun HeaderBarButton(
     modifier: Modifier = Modifier,
     onClick: () -> Unit = {},
-    filePath: String = "Icons/GearIcon.png"
+    filePath: String
 ) {
-
-    Box(
-        modifier = modifier
-            .fillMaxHeight()
-            .aspectRatio(1.0f)
-            .clip(CircleShape)
-            .clickable(onClick = onClick)
-    ) {
         AsyncImage(
             model = "file:///android_asset/${filePath}",
             contentDescription = null,
             modifier = Modifier
-                .fillMaxSize()
+                .size(70.dp)
+                .clickable(onClick = onClick)
         )
-    }
 }
 
 @Preview
@@ -86,32 +71,18 @@ fun HeaderBar(setCurrentPage: (Pages) -> Unit = {}) {
     var textHeight by remember { mutableStateOf(0) }
     val density = LocalDensity.current.density
 
-    Box(
-        modifier = Modifier
-            .background(Theme.NyagiGreen)
-            .fillMaxWidth()
-            .height(80.dp)
-            .padding(10.dp)
-            .onGloballyPositioned { coordinates ->
-                textHeight = coordinates.size.height
-            },
-    ) {
 
-        Row() {
-            HeaderBarButton(filePath = "Icons/BackIcon.png")
-            Spacer(Modifier.width(5.dp))
-            Text(
-                text = "NYAGI",
-                color = Theme.Black,
-                style = TextStyle(
-                    fontSize = (textHeight / density).sp,
-                    fontWeight = FontWeight.Bold,
-                    fontStyle = FontStyle.Italic,
-                ),
-            )
-            Spacer(modifier = Modifier.weight(1f))
-            HeaderBarButton(onClick = { setCurrentPage(Pages.Settings) })
-        }
+    Row(modifier = Modifier
+        .background(Theme.NyagiGreen)
+        .fillMaxWidth()
+        .height(80.dp)
+        .padding(horizontal = Theme.StandardPageMargin)
+        .onGloballyPositioned { coordinates -> textHeight = coordinates.size.height },
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        HeaderBarButton(filePath = "Icons/BackIcon.png")
+        HeaderBarButton(onClick = { setCurrentPage(Pages.Settings)}, filePath = "Icons/GearIcon.png")
     }
 }
 
@@ -132,7 +103,7 @@ fun DevelopmentScreen(uiState: UiState, onImageProxyAnalyzed: (ImageProxy) -> Un
     HeaderBar(setCurrentPage = setCurrentPage)
 
     // include any modifier that applies to any page here
-    val defaultModifier = Modifier.padding(Theme.StandardPageMargin)
+    val defaultModifier = Modifier.padding(horizontal = Theme.StandardPageMargin)
 
     when (currentPage) {
         Pages.BodyRegions -> {
