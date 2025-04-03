2307804569734c54d09748f833246647cd0f40f5
Ethan Nguyen <etng4880@colorado.edu>
2025-04-03 13:16:50 -0600

    re-added header to body part selector, formatted body part selector box

--- app/src/main/java/com/google/aiedge/examples/imageclassification/pages/BodyRegionsPage.kt
+++ app/src/main/java/com/google/aiedge/examples/imageclassification/pages/BodyRegionsPage.kt
@@ -25,9 +25,15 @@ import coil.request.ImageRequest
 import coil.size.Size.Companion.ORIGINAL
 import com.google.aiedge.examples.imageclassification.view.Theme
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.offset
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.google.aiedge.examples.imageclassification.MainViewModel
 import com.google.aiedge.examples.imageclassification.UiState
+import com.google.aiedge.examples.imageclassification.language.SettingsPageText
 import com.google.aiedge.examples.imageclassification.navigation.Pages
+import com.google.aiedge.examples.imageclassification.view.TextHeader
 
 // include all implemented model options here
 private val options:List<String> = listOf("Breast", "Pregnancy", "Early Pregnancy", "Late Pregnancy", "Shoulder")
@@ -60,41 +66,56 @@ fun SelectorOption(optionName:String, mainViewModel: MainViewModel) {
 //        modifier = Modifier
 //            .size(300.dp)
 //    )
+    // in dp
+    val buttonSize = 180
+    val textboxHeight = 50
+
     Box(Modifier
-        .height(300.dp)
-        .width(300.dp)
+        .height(buttonSize.dp)
+        .width(buttonSize.dp)
         .clip(RoundedCornerShape(10.dp))
         .background(Theme.Purple)
         .paint(painter, contentScale = ContentScale.FillBounds)
-        .border(3.dp, Theme.Black, shape = RoundedCornerShape(10.dp)
-        )
-    ) {
+        .border(3.dp, Theme.Grey, shape = RoundedCornerShape(10.dp)))
+    {
         CameraButton(onClick = { mainViewModel.pushPage(Pages.Scan) })
-        Box(
-            Modifier
-                .fillMaxWidth()
-                .border(3.dp, Theme.Black, shape = RoundedCornerShape(10.dp)),
+        Box(Modifier
+            .offset(x=0.dp, y=(buttonSize-textboxHeight).dp) // sits this at the bottom of the box
+        ) {
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .height(textboxHeight.dp)
+                    .background(Theme.White)
+                    .border(3.dp, Theme.Grey, shape = RoundedCornerShape(10.dp))
 
-        ){
-            Text("Test text String")
+            ){
+                Text(optionName,
+                    modifier = Modifier.align(Alignment.Center),
+                    fontSize = 20.sp)
+            }
         }
     }
 }
 
-@Composable
-fun OptionsGrid(){
-    LazyVerticalGrid(columns = GridCells.Fixed(2)) {
-        items(options){ option ->
-//            SelectorOption(option)
-        }
-    }
-}
+//@Composable
+//fun OptionsGrid(){
+//    LazyVerticalGrid(columns = GridCells.Fixed(2)) {
+//        items(options){ option ->
+////            SelectorOption(option)
+//        }
+//    }
+//}
 
 // modifier here is standard modifier that applies to every page
 @Composable
 fun BodyRegionsPage(currentLanguage: Language, modifier: Modifier, mainViewModel: MainViewModel) {
     Column(modifier = modifier) {
 //        OptionsGrid()
+        // Update settings text with the current language
+        TextHeader("Body Part Selector") // TEMP HARD CODED STRING
+//        val context = LocalContext.current
+//        TextHeader(SettingsPageText.getSettings(context, currentLanguage))
         SelectorOption("Breast", mainViewModel)
     }
 }
