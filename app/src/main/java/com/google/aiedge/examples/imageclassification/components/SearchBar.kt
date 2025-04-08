8a9fe902a716390195faeb41e636f8a52b2171a2
Ethan Nguyen <etng4880@colorado.edu>
2025-04-07 19:01:25 -0600

    added date header component code, ultrasound image template code

--- app/src/main/java/com/google/aiedge/examples/imageclassification/components/SearchBar.kt
+++ app/src/main/java/com/google/aiedge/examples/imageclassification/components/SearchBar.kt
@@ -15,9 +15,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import coil.size.Size.Companion.ORIGINAL
 import com.google.aiedge.examples.imageclassification.toAndroidPath
 import com.google.aiedge.examples.imageclassification.view.Theme
 
--- app/src/main/java/com/google/aiedge/examples/imageclassification/pages/GalleryPage.kt
+++ app/src/main/java/com/google/aiedge/examples/imageclassification/pages/GalleryPage.kt
@@ -1,23 +1,115 @@
 package com.google.aiedge.examples.imageclassification.pages
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import coil.size.Size.Companion.ORIGINAL
 import com.google.aiedge.examples.imageclassification.components.SearchBar
 import com.google.aiedge.examples.imageclassification.data.GalleryImage
 import com.google.aiedge.examples.imageclassification.data.GalleryImages
 import com.google.aiedge.examples.imageclassification.language.GalleryText
 import com.google.aiedge.examples.imageclassification.language.Language
+import com.google.aiedge.examples.imageclassification.view.ContentDivider
 import com.google.aiedge.examples.imageclassification.view.TextHeader
 import com.google.aiedge.examples.imageclassification.view.Theme
 
+// template for date headers. change as needed
+@Composable
+fun DateHeader(date: String){
+    Column(Modifier
+        .fillMaxWidth())
+    {
+        Spacer(Modifier.height(10.dp))
+        Text(date,
+            fontSize = 15.sp,
+            fontWeight = FontWeight.Bold)
+        Spacer(Modifier.height(5.dp))
+        ContentDivider(Theme.Grey)
+        Spacer(Modifier.height(10.dp))
+    }
+}
+
+// this is a template for individual image tiles. change data types/input as needed.
+@Composable
+fun ImageTile(imageFile: String, time: String, imageLabel: String){
+    val painter = rememberAsyncImagePainter(
+        model = ImageRequest.Builder(LocalContext.current)
+            // this will need to be changed based off how these images are stored
+            .data("file:///android_asset/${imageFile}")
+            .size(ORIGINAL)
+            .build(),
+        contentScale = ContentScale.Fit,
+    )
+    var boxHeight = 150
+    var boxWidth = 170
+    var textAreaHeight = 30
+    var textMargin = 10
+
+    Box(Modifier
+        .height(boxHeight.dp)
+        .width(boxWidth.dp)
+        .clip(RoundedCornerShape(10.dp))
+        .paint(painter, contentScale = ContentScale.FillBounds)
+        .border(2.dp, Theme.Grey, shape = RoundedCornerShape(10.dp)))
+    {
+        // contains image label text/time
+        Box(Modifier
+            .offset(x=0.dp, y=(boxHeight-textAreaHeight).dp)
+            .height(textAreaHeight.dp)
+            .background(Theme.White)// sits this at the bottom of the box
+        ){
+            // this row: adds side margins
+            Row(Modifier
+                .height(textAreaHeight.dp)
+                .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Spacer(Modifier.width(textMargin.dp))
+                // this row: actually contains text
+                Row(
+                    Modifier.width(boxWidth.dp)
+                ){
+                    Text(time, fontWeight = FontWeight.Bold)
+                    Spacer(Modifier.width((textMargin*2).dp))
+                    Text(imageLabel,
+                        Modifier
+                            .width((boxWidth/3).dp)
+                            .height((textAreaHeight/2).dp))
+                }
+                Spacer(Modifier.width(textMargin.dp))
+            }
+        }
+    }
+}
+
 @Preview
+@Composable
+fun ImageTileExample(){
+    ImageTile("mal_test.png", "10:30:32am", "No label")
+}
+@Preview
+@Composable
+fun DateHeaderPreview(){
+    DateHeader("May 2, 2024")
+}
+
 @Composable
 fun GalleryPage(currentLanguage: Language = Language.ENGLISH, modifier: Modifier) {
 
@@ -47,5 +139,10 @@ fun GalleryPage(currentLanguage: Language = Language.ENGLISH, modifier: Modifier
                 )
             }
         }
+
+        // this displays components in actions as a template. delete these
+        // and implement them appropriately
+        DateHeader("May 2, 2024 - THIS AND THE FOLLOWING IMAGE ARE A HARDCODED EXAMPLE")
+        ImageTile("mal_test.png", "10:30:32am", "No label")
     }
 }
